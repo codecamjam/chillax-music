2834e5ee27bbeedcaf21c3d5483086823719a786
codecamjam <camerontaghlabi@gmail.com>
2021-08-20 02:26:43 -0400

    services completed

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,9 +5,14 @@ import Streaming from "../sections/streaming/Streaming";
 import "./Services.scss";
 const Services = () => {
   return (
-    <Container className="services p-0 m-0" fluid={true}>
-      <Row className="w-100 m-0 p-0 justify-content-between">
-        <Col xs={12} lg={6} xl={4} className="my-2 mx-0 p-0">
+    <Container className="services p-0" fluid={true}>
+      <Row className="w-100 m-0 p-0">
+        <Col
+          xs={12}
+          lg={6}
+          xl={{ span: 4, offset: 12 }}
+          className="my-2 mx-0 p-0"
+        >
           <Streaming />
         </Col>
         <Col xs={12} lg={6} xl={4} className="my-2 mx-0 p-0">
